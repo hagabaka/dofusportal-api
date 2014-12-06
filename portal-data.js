85d8cffadb6ffb07761976e4ad235aa9cb446ce0
Yaohan Chen <yaohan.chen@gmail.com>
2014-12-06 14:58:38 -0500

    Split scanning code into its own function

--- portal-data.js
+++ portal-data.js
@@ -15,26 +15,31 @@ var capitalize = function(string) {
   });
 };
 
+function scanForCoordinates(text, found) {
+  var dimensionsMentioned = text.match(dimensionName);
+  var coordinatesMentioned = text.match(coordinates);
+  if(dimensionsMentioned && coordinatesMentioned &&
+     dimensionsMentioned.length === coordinatesMentioned.length) {
+    dimensionsMentioned.forEach(function (dimension, index) {
+      var coordinates = '[' + coordinatesMentioned[index].match(/-?\d+/g).join(',') + ']';
+      found(capitalize(dimension), coordinates);
+    });
+  } else {
+    console.error('Cannot understand post: ' + text);
+  }
+}
+
 function appendData(parsedPage, portals) {
   parsedPage.posts().reverse().forEach(function(post) {
-    text = post.body;
-    var dimensionsMentioned = text.match(dimensionName);
-    var coordinatesMentioned = text.match(coordinates);
-    if(dimensionsMentioned && coordinatesMentioned &&
-       dimensionsMentioned.length === coordinatesMentioned.length) {
-      dimensionsMentioned.forEach(function (dimension, index) {
-        portals[capitalize(dimension)].push({
-          coordinates: '[' + coordinatesMentioned[index].match(/-?\d+/g).join(',') + ']',
-          postingDate: post.postingDate,
-          author: post.author,
-          likes: post.likes
-        });
+    scanForCoordinates(post.body, function(dimension, coordinates) {
+      portals[capitalize(dimension)].push({
+        coordinates: coordinates,
+        postingDate: post.postingDate,
+        author: post.author,
+        likes: post.likes
       });
-    } else {
-      console.error('Cannot understand post: ' + post.body);
-    }
+    });
   });
-
 }
 
 exports.portalData = function(url) {
