55f18c1dae43fe69353c78c9d7f3ca35cea38d1f
Yaohan Chen <yaohan.chen@gmail.com>
2014-12-11 22:44:24 -0500

    Compare data with JSON string rather than toString

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function handleServer(name, url) {
 
   setInterval(function() {
     var newData = portalData(url);
-    if(newData.toString() !== data.toString()) {
+    if(JSON.stringify(newData) !== JSON.stringify(data.toString())) {
       data = newData;
       sse.sender()(data);
     }
