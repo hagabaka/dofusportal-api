ace2c6220e800e291dd53649c4268f5392bc15ee
Yaohan Chen <yaohan.chen@gmail.com>
2015-01-02 14:55:38 -0500

    Switch to redis storage

--- storage.js
+++ storage.js
@@ -1,28 +1,19 @@
-var memjs = require('memjs');
-
-var client = memjs.Client.create();
-
-if (!client.stats()) {
-  var data = {};
-  client = {
-    get: function(key, callback) {
-      return callback(data[key] || 'null');
-    },
-    set: function(key, value, callback) {
-      data[key] = value;
-      callback(value);
-      return value;
-    }
-  };
-}
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 
 exports.get = function(key, callback) {
-  client.get(key, function(data) {
+  console.log('Getting from storage ', key);
+  client.get(key, function(error, data) {
+    console.log('Got from storage ', key, data);
     callback(JSON.parse(data));
   });
 };
 
 exports.set = function(key, data, callback) {
+  console.log('Storing ', key, data);
   client.set(key, JSON.stringify(data), callback || function() {});
 };
 
