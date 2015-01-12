93500aaedf5a814dffddbb6761e83b71e8031308
Yaohan Chen <yaohan.chen@gmail.com>
2015-01-12 02:21:42 -0500

    Scan posts line by line

--- parser.js
+++ parser.js
@@ -8,45 +8,29 @@ var areas = names.areas;
 var areaPattern = new RegExp(areas.join('|'), 'i');
 
 module.exports = function scanForCoordinates(text, found) {
-  var nextDimension;
-  var workingText = text;
-  var spellChecker = new levenshtein(workingText.split(/\s+/));
+  var spellChecker = new levenshtein(text.split(/\s+/));
   dimensions.concat(areas).forEach(function(name) {
-    workingText = workingText.replace(new RegExp(name, 'i'), name);
+    text = text.replace(new RegExp(name, 'i'), name);
     spellChecker.find(name, function(misspelled) {
-      workingText = workingText.replace(misspelled, name);
+      text = text.replace(misspelled, name);
     });
   });
-  workingText = workingText.replace(/\bxel\b/i, 'Xelorium');
-  do {
-    var dimensionMatch = workingText.match(dimensionPattern);
-    if(dimensionMatch) {
-      var dimension = dimensionMatch[0];
-      var indexAfter = dimensionMatch.index + dimension.length;
-      var textAfter = workingText.substr(indexAfter);
-      nextDimension = textAfter.match(dimensionPattern);
-      if(nextDimension) {
-        workingText = workingText.substr(0, indexAfter + nextDimension.index);
-        nextWorkingText = textAfter.slice(nextDimension.index);
-      } else {
-        nextWorkingText = textAfter;
-      }
-      var coordinates = grep(workingText, /(-?\d+),\s*(-?\d+)/, function(_, x, y) {
-        return '[' + x + ',' + y + ']';
+  text = text.replace(/\bxel\b/i, 'Xelorium');
+
+  text.split(/\r?\n/).forEach(function(line) {
+    var dimension = grep(line, dimensionPattern);
+    var area = grep(line, areaPattern);
+    var coordinates = grep(line, /(-?\d+),\s*(-?\d+)/, function(_, x, y) {
+      return '[' + x + ',' + y + ']';
+    });
+    var uses = grep(line, /\(?(\d+)\)?\s+uses/, function(_, number) {
+      return parseInt(number);
+    });
+    if(dimension && coordinates) {
+      found(dimension, coordinates, {
+        area: area,
+        uses: uses
       });
-      if(coordinates) {
-        var area = grep(workingText, areaPattern);
-        var uses = grep(workingText, /\(?(\d+)\)?\s+uses/, function(_, number) {
-          return parseInt(number);
-        });
-        found(dimension, coordinates, {
-          area: area,
-          uses: uses
-        });
-      } else {
-        console.log('Missing coordinates', workingText, text);
-      }
-      workingText = nextWorkingText;
     }
-  } while(workingText.length > 0 && nextDimension);
-}
+  });
+};
